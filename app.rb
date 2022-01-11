ba8fe1e1e3ea75013716b8b81601dd3f9c3cc428
Orla <orladunlop2@gmail.com>
2022-01-11 16:02:38 +0000

    First commit - set up

---
+++ app.rb
@@ -0,0 +1,17 @@
+require "sinatra/base"
+require "sinatra/reloader"
+
+class Battle < Sinatra::Base
+  configure :development do 
+    register Sinatra::Reloader
+  end
+
+  get "/" do
+    "Hello Battle!"
+  end
+
+  # start the server if ruby file executed directly
+  # $0 = what was last in the command line
+  # config file is run when rackup is called
+  run! if app_file == $0
+end
